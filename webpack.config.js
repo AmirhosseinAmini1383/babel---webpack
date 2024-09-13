0300af2ecc1972c389b07fa7ccccc4ebf4860e95
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-09-13 16:39:06 +0330

    minimizing and usglifying code

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-
+const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "/src/js/app.js",
@@ -32,5 +32,16 @@ module.exports = {
     compress: true,
     port: 3000,
     open: true,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+        },
+      }),
+    ],
   },
 };
